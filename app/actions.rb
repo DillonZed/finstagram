1f700eb121b95a30775118bc5e8fc7eb698ba150
Dillon Zolnierczyk <dillon@theconvertlab.com>
2021-11-30 04:31:04 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,21 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
 end
 
+# Sign up form
 get '/signup' do
   @user=User.new
   erb :signup
 end
 
+# Sign in the user
 post '/signup' do
   email = params[:email]
   avatar_url = params[:avatar_url]
@@ -17,8 +25,34 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
 
   if @user.save
-    "User #{username} Saved!"
+    redirect to('/login')
   else
-   erb(:signup)
+    erb(:signup)
   end
+end
+
+# Login form
+get '/login' do
+  erb :login
+end
+
+# Login the user
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+@user = User.find_by(username: username)
+
+if @user && @user.password == password
+  session[:user_id] = @user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
